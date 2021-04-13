92e27747dfabb956613a42acb302e8789b52e8d7
kenken-9 <1508.parfum@gmail.com>
2021-04-13 12:35:12 +0900

    バリエーションの記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+
+def new
+end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,24 @@ class Product < ApplicationRecord
     has_one_attaced :image
 
     extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category, :condition, :fee, :area, :ship
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :fee
+    belongs_to :area
+    belongs_to :ship
+
+    with_options presence: true do
+        validates :image
+        validates :name
+        validates :text
+    end
+
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :fee_id, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :ship_id, numericality: { other_than: 1 }
+
+
+
 end
