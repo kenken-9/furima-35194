0a59af1e98a46306919c3e452ae7b96a64b2b83f
kenken-9 <1508.parfum@gmail.com>
2021-04-13 12:15:07 +0900

    ActiveHashの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
     has_one_attaced :image
+
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category, :condition, :fee, :area, :ship
 end
