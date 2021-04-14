56ee9ffd1e45c7c1a7deac73db86879b5bf6c53e
kenken-9 <1508.parfum@gmail.com>
2021-04-14 17:35:40 +0900

    販売手数料、販売利益の記述

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -1,13 +1,23 @@
-window.addEventListener("load", function(){
+function priceAdd() {
 
     const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+    
     priceInput.addEventListener("input", () => {
         const inputValue = priceInput.value;
         console.log(inputValue);
+        const addTax = Math.floor(inputValue * 0.1);
+        addTaxDom.innerHTML = addTax
+        profitDom.innerHTML = Math.floor(inputValue - addTax);
     })
+    
+    
+}
+
+window.addEventListener("load", priceAdd)
 
 
-})    
 
 
 
