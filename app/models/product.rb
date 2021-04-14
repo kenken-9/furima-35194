db94b1c65bdf5d55bdc67850caa9d2eacfc3d3cd
kenken-9 <1508.parfum@gmail.com>
2021-04-14 18:02:23 +0900

    with_optionsでバリエーションのまとめ

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,26 +1,27 @@
 class Product < ApplicationRecord
-    has_one_attached :image
+  has_one_attached :image
 
-    belongs_to :user
+  belongs_to :user
 
-    extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :condition
-    belongs_to :fee
-    belongs_to :area
-    belongs_to :ship
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :ship
 
-    with_options presence: true do
-        validates :image
-        validates :name 
-        validates :text
-        validates :price, format: { with: /\A[0-9]+\z/ , message: "Half-width number" }, inclusion: { in: 300..9999999,message: "Out of setting range"} 
-    end
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :text
+    validates :price, format: { with: /\A[0-9]+\z/, message: "Half-width number" }, inclusion: { in: 300..9999999, message: "Out of setting range" }
+  end
 
-    validates :category_id, numericality:  {other_than: 1 ,message: "Select"}
-    validates :condition_id, numericality: {other_than: 1 ,message: "Select"}
-    validates :fee_id, numericality: {other_than: 1 ,message: "Select"}
-    validates :area_id, numericality: {other_than: 1 ,message: "Select"}
-    validates :ship_id, numericality: {other_than: 1 ,message: "Select"}
-
+  with_options numericality: { other_than: 1, message: "Select" } do
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :area_id
+    validates :ship_id
+  end
 end
