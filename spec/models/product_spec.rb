fa7c6ac80547a4ec5c9e82b3df35fe84c137aef3
kenken-9 <1508.parfum@gmail.com>
2021-04-14 18:08:48 +0900

    クオーテーションの削除

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -38,49 +38,49 @@ RSpec.describe Product, type: :model do
       end
 
       it "カテゴリーを選択しないと登録できない" do
-        @product.category_id = "1"
+        @product.category_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Category Select")
       end
 
       it "商品の状態を選択しないと登録できない" do
-        @product.condition_id = "1"
+        @product.condition_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Condition Select")
       end
 
       it "発送料の負担を選択しないと登録できない" do
-        @product.fee_id = "1"
+        @product.fee_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Fee Select")
       end
 
       it "発送元の地域を選択しないと登録できない" do
-        @product.area_id = "1"
+        @product.area_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Area Select")
       end
 
       it "発送までの日数を選択しないと登録できない" do
-        @product.ship_id = "1"
+        @product.ship_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Ship Select")
       end
 
       it "販売価格が空では登録できない" do
-        @product.price = ""
+        @product.price = nil
         @product.valid?
         expect(@product.errors.full_messages).to include("Price can't be blank")
       end
 
       it "販売価格は299円以下では登録できない" do
-        @product.price = "299"
+        @product.price = 299
         @product.valid?
         expect(@product.errors.full_messages).to include("Price Out of setting range")
       end
 
       it "販売価格は10,000,000円以上では登録できない" do
-        @product.price = "10000000"
+        @product.price = 10000000
         @product.valid?
         expect(@product.errors.full_messages).to include("Price Out of setting range")
       end
