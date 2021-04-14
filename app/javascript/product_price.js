d9872ddef644b54baca9d008dbfb9952c18a6d6f
kenken-9 <1508.parfum@gmail.com>
2021-04-14 16:27:32 +0900

    priceのイベント発火処理

---
+++ app/javascript/product_price.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(){
+
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
+        const inputValue = priceInput.value;
+        console.log(inputValue);
+    })
+
+
+})    
+
+
+
