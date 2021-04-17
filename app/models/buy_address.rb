a33f13d3bf52bbf2a0449e1dd83a84bf2087a603
kenken-9 <1508.parfum@gmail.com>
2021-04-17 15:44:52 +0900

    Formオブジェクトの作成

---
+++ app/models/buy_address.rb
@@ -0,0 +1,20 @@
+class BuyAddress
+    include ActiveModel: :Model
+    attr_accessor :user_id, :product_id, :postal_code, :area_id, :municipality, :block_number, :building_name, :phone_number, :buy_id
+
+    with_options presence:true do
+      validates :user_id
+      validates :product_id
+      validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "code Input correctly"}
+      validates :municipality
+      validates :block_number
+      validates :phone_number, format: {with: /\A\d{11}\z/ , message: "Input only number"}
+      validates :buy_id
+    end
+    validates :area_id, numericality: { other_than: 1, message: "Select" }
+
+    def save
+      buy = Buy.create(user_id: user_id, product_id: product_id)
+      Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, block_number: block_number, building_name: building_name, phone_number: phone_number, buy_id: buy.id )
+    end
+end
