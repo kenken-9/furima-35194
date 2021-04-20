3ee564b265fba624779a481b027503448e836520
kenken-9 <1508.parfum@gmail.com>
2021-04-20 19:31:25 +0900

    user_idの追加

--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -9,6 +9,7 @@ class BuyAddress
       validates :block_number
       validates :phone_number, format: {with: /\A\d{11}\z/ , message: "Input only number"}
       validates :token
+      validates :user_id
     end
     validates :area_id, numericality: { other_than: 1, message: "Select" }
 
