bfdfe13cc200aceb8d39af7119389c7b0d76cdfa
kenken-9 <1508.parfum@gmail.com>
2021-04-20 19:17:40 +0900

    authenticate_user!にcreateの追加

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,6 @@
 class BuysController < ApplicationController
   before_action :set_product , only:[:create, :index]
-  before_action :authenticate_user!, only:[:index]
+  before_action :authenticate_user!, only:[:index, :create]
   before_action :set_path, only:[:index]
 
   def index
